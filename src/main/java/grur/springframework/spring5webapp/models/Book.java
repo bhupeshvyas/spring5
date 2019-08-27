8ae2f46b7f92384dc5cd2a328eb2d119c37e3474
vyasb <bhupesh.vyas@msg-global.com>
2019-08-27 16:53:26 +0530

    added controller for author and book and added thyemleaf templates for both
    added controller for author and book and added thyemleaf templates for both

--- src/main/java/grur/springframework/spring5webapp/models/Book.java
+++ src/main/java/grur/springframework/spring5webapp/models/Book.java
@@ -40,6 +40,14 @@ public class Book {
         return title;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -85,14 +93,5 @@ public class Book {
         return Objects.hash(id);
     }
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", publisher=" + publisher +
-                ", authors=" + authors +
-                '}';
-    }
+
 }
